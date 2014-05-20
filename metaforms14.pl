1d762b6fa9671b48fb6528f22a76492dc22a303e
verdefred <verdefred@hotmail.com>
2014-05-20 01:47:54 +0100

    A por Peca

--- metaforms14.pl
+++ metaforms14.pl
@@ -12,43 +12,43 @@
 % ESCREVER AQUI O CODIGO DO PROJECTO DE LP 2013/2014
 
 trioLeft(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloVertical(A, Linha, Coluna, Tabuleiro)). 
+  (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)). 
 
 trioRight(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloVertical(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)).
 
 cobra(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloVertical(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)).
 
 tSimples(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloHorizontal(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloHorizontal(Peca, Linha, Coluna, Tabuleiro)).
 
 tLeft(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloVertical(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)).
 
 tRight(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloVertical(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)).
 
 tInvertido(Peca, Linha, Coluna, Tabuleiro) :-
-  (rectanguloHorizontal(A, Linha, Coluna, Tabuleiro)).
+  (rectanguloHorizontal(Peca, Linha, Coluna, Tabuleiro)).
 
 cantoTopLeft(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 cantoTopRight(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 cantoBottomLeft(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 cantoBottomRight(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
-  (quadrado(A, Linha, Coluna, Tabuleiro)).
+  (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
 
 
