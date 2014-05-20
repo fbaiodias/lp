a4df461df5a0d37d2ab72fe64c5681b673da5aa6
verdefred <verdefred@hotmail.com>
2014-05-20 20:47:59 +0100

    Lindo

--- metaforms14.pl
+++ metaforms14.pl
@@ -1,8 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %
-%       GRUPO NUM: 
-%       ALUNOS: 
+%       GRUPO NUM: 69
+%       ALUNOS: FRANCISCO DIAS E FREDERICO MOURA 
 %
 %               ATENCAO: NAO USAR ACENTOS OU CEDILHAS
 %
@@ -10,6 +10,9 @@
 
 
 % ESCREVER AQUI O CODIGO DO PROJECTO DE LP 2013/2014
+
+% Definicao das pecas para depois implementar no bagof
+% Todas as pecas possiveis de encontrar
 
 peca(triangulo, azul).
 peca(triangulo, amarelo).
@@ -20,6 +23,10 @@ peca(circulo, vermelho).
 peca(quadrado, azul).
 peca(quadrado, amarelo). 
 peca(quadrado, vermelho).
+
+% Pistas intermedias implementadas recorrendo aos predicados do metaforms12
+% Por exemplo, no caso seguinte, do trioLeft sabemos que e incluido no
+% rectanguloVertical.
 
 trioLeft(Peca, Linha, Coluna, Tabuleiro) :-
   (rectanguloVertical(Peca, Linha, Coluna, Tabuleiro)). 
@@ -60,10 +67,16 @@ diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :-
 diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
   (quadrado(Peca, Linha, Coluna, Tabuleiro)).
 
+% Aqui usamos o bagof para encontrar todas as pecas sendo X o tipo de peca 
+% (triangulo, circulo ou quadrado) e Y a cor (azul, amarelo, vermelho) que
+%  as poe dentro da lista TodasPecas.
 
 encontraTodasPecas(TodasPecas) :- 
   bagof(peca(X,Y), peca(X,Y), TodasPecas).
 
+% No check, primeiro pomos todas as pecas dentro da lista TodasPecas e depois,
+% vendo as permutacoes possiveis para a lista Todas pecas (9 factorial), compara
+% com o tabuleiro e devolve uma solucao possivel (que encaixe no Tabuleiro)
 
 check(Tabuleiro, TabuleiroTotal) :- 
   encontraTodasPecas(TodasPecas), 
